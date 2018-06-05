d868b27620957af8783a6568a6f076b5c9d16fb0
agnes musyoka <kavatamusyoka24@gmail.com>
2018-06-05 16:02:55 +0300

    add changes

---
+++ feature/src/test/java/com/example/agnes/myrestaurants/feature/MainActivityTest.java
@@ -0,0 +1,19 @@
+package com.example.agnes.myrestaurants.feature;
+
+
+//import org.junit.runner.RunWith;
+//import org.robolectric.Robolectric;
+//import org.robolectric.RobolectricGradleTestRunner;
+//import org.robolectric.annotation.Config;
+//
+//@Config(constants = BuildConfig.class, sdk = Build.VERSION_CODES.LOLLIPOP)
+//@RunWith(RobolectricGradleTestRunner.class)
+//
+//public class MainActivityTest {
+//    private MainActivity activity;
+//
+//    @Before
+//    public void setup() {
+//        activity = Robolectric.setupActivity(MainActivity.class);
+//    }
+//}
