726651461ae731cb041bede143e7d5756009e495
agnes musyoka <kavatamusyoka24@gmail.com>
2018-06-05 09:24:38 +0300

    add background images

--- feature/src/main/java/com/example/agnes/myrestaurants/feature/MainActivity.java
+++ feature/src/main/java/com/example/agnes/myrestaurants/feature/MainActivity.java
@@ -1,13 +1,20 @@
+
+
 package com.example.agnes.myrestaurants.feature;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button mFindRestaurantsButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mFindRestaurantsButton = (Button) findViewById(R.id.findRestaurantsButton);
+
     }
 }
