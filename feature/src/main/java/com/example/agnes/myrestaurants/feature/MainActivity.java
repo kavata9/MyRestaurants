120f589bcf572956102c730bf5629fe2f9108c33
agnes musyoka <kavatamusyoka24@gmail.com>
2018-06-05 10:34:47 +0300

    add intent function to move to a new activity

--- feature/src/main/java/com/example/agnes/myrestaurants/feature/MainActivity.java
+++ feature/src/main/java/com/example/agnes/myrestaurants/feature/MainActivity.java
@@ -1,7 +1,7 @@
 
 
 package com.example.agnes.myrestaurants.feature;
-
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -22,7 +22,8 @@ public class MainActivity extends AppCompatActivity {
         mFindRestaurantsButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "Hello World!", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(MainActivity.this, RestaurantsActivity.class);
+                startActivity(intent);
             }
         });
 
